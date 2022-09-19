bbfeca37c6a1fd23741009f291d7bdf1969ba36c
labibaadinda <100503649+labibaadinda@users.noreply.github.com>
2022-09-19 09:30:24 +0700

    Add files via upload

---
+++ submission-1/24060121140111/boolean.h
@@ -0,0 +1,15 @@
+#ifndef boolean_H
+#define boolean_H
+/* Program   : */
+/* Program   : boolean.h */
+/* Deskripsi : */
+/* NIM/Nama  : Labiba Adinda Zahwana/ 24060121140111*/
+/* Tanggal   : 18 September 2022*/
+/***********************************/
+
+//type boolean macro bahasa C, false=0, true=1
+#define false 0
+#define true  1
+#define boolean unsigned char
+
+#endif
