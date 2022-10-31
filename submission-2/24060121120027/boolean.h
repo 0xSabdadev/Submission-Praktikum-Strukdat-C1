b7df0d2c5bac45cff07fb5a2389408c672dac7ef
novidf <100180727+novidf@users.noreply.github.com>
2022-10-31 07:46:03 +0700

    ListLinear
    Novi Dwi Fitriani - 24060121120027

---
+++ submission-2/24060121120027/boolean.h
@@ -0,0 +1,15 @@
+#ifndef boolean_H
+#define boolean_H
+/***********************************/
+/* Program   : boolean.h */
+/* Deskripsi : file HEADER modul boolean */
+/* NIM/Nama  : 24060121120027/ Novi Dwi Fitriani */
+/* Tanggal   : 24 Oktober 2022 */
+/***********************************/
+
+//type boolean macro bahasa C, false=0, true=1
+#define false 0
+#define true  1
+#define boolean unsigned char
+
+#endif
