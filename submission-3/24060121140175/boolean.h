ba45d601ec925854c2e083aba217fc72e48c1ae6
Maulana Pasya Zefanya <88534377+maulanapasya@users.noreply.github.com>
2022-11-28 08:00:59 +0700

    Tugas3_Maulana Pasya Zefanya_24060121140175
    Permisi kak, izin mengumpulkan tugas 3 materi list double

---
+++ submission-3/24060121140175/boolean.h
@@ -0,0 +1,14 @@
+#ifndef boolean_H
+#define boolean_H
+/* Program   : boolean.h */
+/* Deskripsi : */
+/* NIM/Nama  : Maulana Pasya Zefanya/ 24060121140175*/
+/* Tanggal   : 31 Oktober 2022*/
+/***********************************/
+
+//type boolean macro bahasa C, false=0, true=1
+#define false 0
+#define true  1
+#define boolean unsigned char
+
+#endif
